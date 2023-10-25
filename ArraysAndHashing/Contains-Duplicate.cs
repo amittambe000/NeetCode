22248880e0776cf2eaedaa540aa3846be9ba020d
Amit Tambe <amittambe0000@gmail.com>
2023-10-25 22:09:48 +0530

    NeetCode- Contains Duplicate

---
+++ ArraysAndHashing/Contains-Duplicate.cs
@@ -0,0 +1,43 @@
+﻿namespace ArraysAndHashing;
+/*
+https://leetcode.com/problems/contains-duplicate/
+
+Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
+
+ Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+public class Contains_Duplicate
+{
+
+ public bool ContainsDuplicate(int[] nums)
+ {
+  HashSet<int> result = new HashSet<int>();
+
+  if (nums.Length == 1)
+   return false;
+
+  foreach (var num in nums)
+  {
+   if (result.Contains(num))
+    return true;
+   else
+   {
+    result.Add(num);
+   }
+  }
+  return false;
+ }
+    
+}
---
+++ ArraysAndHashing/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using ArraysAndHashing;
+
+Console.WriteLine("Arrays and Hashing!");
+
+var duplicate = new Contains_Duplicate().ContainsDuplicate(new int[] {1, 2, 3, 4});
+Console.WriteLine(duplicate);
